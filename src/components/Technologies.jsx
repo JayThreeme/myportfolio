e361ab4b26c26caef7233678b65eeed4293c8fde
JayThreeme <jayjucarjr@gmail.com>
2024-11-13 02:35:03 +0800

    icons updates

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -7,12 +7,11 @@ const Technologies = () => {
     <>
       <div className="flex flex-row flex-wrap justify-center gap-10">
         {technologiesInfo.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
-            <img
-              src={technology.icon}
-              alt={technology.name}
-              className="w-full h-full rounded-full border-4 border-white object-cover"
-            />
+          <div
+            className="w-24 h-24 rounded-full border-4 border-purple-950 bg-gray-200 flex justify-center items-center"
+            key={technology.name}
+          >
+            <img src={technology.icon} alt={technology.name} className="w-14" />
           </div>
         ))}
       </div>
